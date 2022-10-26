827eaca6dfe92ad902b159b145e0b2965f38975d
Gianella Leon <giigl@hotmail.com>
2022-10-26 09:34:58 -0500

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import {Nav} from '../Nav/Nav'
+export const Home =()=>{
+    return(
+        <div>
+           
+            <Nav/>
+           
+        </div>
+    )
+}
+
---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,11 @@
+import './Landing.css'
+export const Landing =()=>{
+    
+    return(
+        <div className='ala'>
+        
+            Landing
+        </div>
+    )
+}
+
