7dccbe531bb72f6a545927a906b7674dc2d69c78
Luis Condori <luis2003nb@gmail.com>
2022-10-26 16:40:51 -0300

    add Link

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,10 +1,12 @@
 import './Landing.css'
+import {Link} from 'react-router-dom'
+
 export const Landing =()=>{
     
     return(
         <div className='ala'>
         
-            Landing
+       <Link to="/home">Landing </Link>     
         </div>
     )
 }
