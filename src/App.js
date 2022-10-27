b09bd36859b67a9fe1a6d74b1f9418440358c71f
Gianella Leon <giigl@hotmail.com>
2022-10-26 21:25:22 -0500

    26

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 import{Routes,Route} from 'react-router-dom'
 import {Landing} from './components/Landing/Landing';
 import {Home} from './components/Home/Home'
-
+import {Perfil} from './components/Perfil/Perfil'
+import{GoPremium} from './components/GoPremium/GoPremium'
 function App() {
   return (
     <div className="App">
       <Routes>
       <Route  path='/' element={<Landing/>}/>
       <Route path='/home' element={<Home/>}/>
+      <Route path='profile/:id' element={<Perfil/>}/>
+      <Route path='/premium' element={<GoPremium/>}/>
       </Routes>
     </div>
   );
---
+++ src/components/GoPremium/GoPremium.jsx
@@ -0,0 +1,8 @@
+import './GoPremium.css'
+export const GoPremium=()=>{
+    return(
+        <div>
+            working on PRemium
+        </div>
+    )
+}
