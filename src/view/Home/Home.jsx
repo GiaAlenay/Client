e39d7b3266cea705f47e117d297f59720074aa42
userJAMT <jmendez4tob@gmail.com>
2022-10-28 15:09:32 -0300

    fix desastre

--- src/view/Home/Home.jsx
+++ src/view/Home/Home.jsx
@@ -1,33 +1,9 @@
-
-
 import { Stack } from '@mui/material'
 import { Feed } from '../../components/Feed/Feed'
 import { Filters } from '../../components/Filters/Filters'
 import {Nav} from '../../components/Nav/Nav'
 import { AddPost } from '../../components/Add/AddPost'
 import { Prueba } from '../../components/Filters/Prueba'
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getUser } from "../../redux/actions/users";
-import { useParams } from 'react-router-dom'
-export const Home =()=>{
-    const dispatch=useDispatch()
-    const { id } = useParams();
-    const User=useSelector(state=>state.User)
-    useEffect(()=>{
-        
-            dispatch(getUser(id))
-            
-        
-        return(()=>{
-            // dispatch(clearAction('VideogameDetail',{}))
-        })
-    },[])
-    return(
-        <div>
-           
-            <Nav User={User}/>
-
 import { useDispatch , useSelector} from "react-redux";
 import {useState , useEffect} from 'react';
 import {getPost, getUsers} from "../../redux/actions/users"
@@ -58,14 +34,11 @@ export const Home =()=>{
         filter? setFilter(false) : setFilter(true)
       }
 
-
     return(
         <div>
            
             <Nav/>
-
             {/* <Stack direction="row" spacing={2} justifyContent="space-between">
-
                 <Filters/>
                 <Feed/>
                 <Prueba/>
@@ -77,7 +50,6 @@ export const Home =()=>{
             </div>
             }
             
-
             {allPost?.map((e)=>{
                 return(
                     <div>
@@ -91,8 +63,6 @@ export const Home =()=>{
             })}
 
 
-
         </div>
     )
 }
-
