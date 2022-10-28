a453a59a1c31d85b0639ddce47dc1c1071215159
Luis Condori <luis2003nb@gmail.com>
2022-10-28 14:39:15 -0300

    ahahh

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,25 +1,29 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 
 
-
+//se puede
 function Cards({titulo,texto,media}){
+
+    
     return(
 
-        <div className={"container"}>
+       
+            <div className={"container"}>
 
             <img className={"img"} src={media} alt="img not found" width="100px" height="100px"/>
-
-            <div className={"textContainer"}>
+            <a href={media} > archivo</a>
+            
 
                 <h4 className={"countryName"}>{titulo}</h4>
-       
+
                 <h4>{texto}</h4>
 
                 <button className={"button"}>Detalle</button>
-                
+
             </div>
-            
-        </div>
+        
+      
     )
 }
 
