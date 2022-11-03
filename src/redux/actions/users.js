340d1a3834d557f9b2925e877b24f7372e798777
Franco Mafferra <francomafferra38@gmail.com>
2022-11-03 15:53:00 -0300

    branch

--- src/redux/actions/users.js
+++ src/redux/actions/users.js
@@ -1,54 +1,53 @@
 import axios from 'axios';
 
 
-  export const getUsers=()=>dispatch=>{
-    return axios.get(`/users`)
-    .then((data)=>{
-        console.log(data.data)
-        dispatch({type:'GET_USERS', payload:data.data})
-    }).catch((error)=>{
-        console.log(error)
-    })
+  export const getUsers=()=>async dispatch=>{
+    try {
+          const data = await axios.get(`/users`);
+          console.log(data.data);
+          dispatch({ type: 'GET_USERS', payload: data.data });
+      } catch (error) {
+          console.log(error);
+      }
 }
 
-export const getUser=(id)=>dispatch=>{
-    return axios.get(`/users/${id}`)
-    .then((data)=>{
-        
-        dispatch({type:'GET_BY_ID', payload: data.data})
-    }).catch((error)=>{
-        console.log(error)
-    })
+export const getUser=(id)=>async dispatch=>{
+    try {
+        const data = await axios.get(`/users/${id}`);
+        dispatch({ type: 'GET_BY_ID', payload: data.data });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-export const deleteUser=(id)=>dispatch=>{
-    return axios.delete(`users/${id}`)
-    .then(d=> {     
-        console.log(d.data.msg) 
-        dispatch({type:'DELETE_USER', payload: d.data.msg})   })
-    .catch(e=> 
-        { console.log(e)
-             })
+export const deleteUser=(id)=>async dispatch=>{
+    try {
+        const d = await axios.delete(`users/${id}`);
+        console.log(d.data.msg);
+        dispatch({ type: 'DELETE_USER', payload: d.data.msg });
+    } catch (e) {
+        console.log(e);
+    }
 }
 
-export const createUser=(user)=>dispatch=>{
-    return axios.post('/users',user)
-    .then(d=> {     
-        console.log(d.data.msg) 
-        dispatch({type:'CREAR_USER', payload: d.data.msg})   })
-    .catch(e=> 
-        { console.log(e)
-             })
+export const createUser=(user)=>async dispatch=>{
+    try {
+        const d = await axios.post('/users', user);
+        console.log(d.data.msg);
+        dispatch({ type: 'CREAR_USER', payload: d.data.msg });
+    } catch (e) {
+        console.log(e);
+    }
 }
 
-export const getUserLoged=(usuario)=>dispatch=>{
-    return axios.get('/users/login',usuario)
-    .then(d=> {     
-        console.log(d.data.user) 
-        dispatch({type:'GET_USER_LOGED', payload: d.data.user})   })
-    .catch(e=> 
-        { console.log(e)
-             })
+export const getUserLoged=(usuario)=>async dispatch=>{
+    try {
+        const d = await axios.get('/users/login', usuario);
+        console.log(d.data.user);
+        dispatch({ type: 'GET_USER_LOGED', payload: d.data.user });
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 
