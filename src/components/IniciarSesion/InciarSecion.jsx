e6bc25f5f58f8c01a7b67fe53673ce092ccb436b
Luis Condori <luis2003nb@gmail.com>
2022-11-12 09:51:33 -0300

    About

--- src/components/IniciarSesion/InciarSecion.jsx
+++ src/components/IniciarSesion/InciarSecion.jsx
@@ -1,85 +1,85 @@
-import { useState, useEffect } from 'react'
-import TextField from '@mui/material/TextField'
-import { useDispatch } from "react-redux";
-import { getUserLoged } from "../../redux/actions/users";
-import './IniciarSesion.css'
-import { useNavigate } from 'react-router-dom';
+// import { useState, useEffect } from 'react'
+// import TextField from '@mui/material/TextField'
+// import { useDispatch } from "react-redux";
+// import { getUserLoged } from "../../redux/actions/users";
+// import './IniciarSesion.css'
+// import { useNavigate } from 'react-router-dom';
 
-function Validate(input,users){
-    const foundusuario=users.find(f=>f.usuario===input.input)        
-    const foundemail=users.find(f=>f.contraseña===input.contraseña)
-    const match=users.find(u=>u.usuario===input.input && u.contraseña===input.contraseña)
-    let errors={};   
-    if(!input.input){errors.input='Usuario o email requerido'}        
-    if(!foundusuario){errors.input='No existe un usario con ese nombre'}   
-    if(!input.contraseña){errors.contraseña='La contraseña es requerida'} 
-    if(!foundemail){errors.contraseña='Contraseña incorrecta.'}
-    if(!match){errors.equivocado='Contraseña o  Usuario incorrecto'}
-    return errors
-}
+// function Validate(input,users){
+//     const foundusuario=users.find(f=>f.usuario===input.input)        
+//     const foundemail=users.find(f=>f.contraseña===input.contraseña)
+//     const match=users.find(u=>u.usuario===input.input && u.contraseña===input.contraseña)
+//     let errors={};   
+//     if(!input.input){errors.input='Usuario o email requerido'}        
+//     if(!foundusuario){errors.input='No existe un usario con ese nombre'}   
+//     if(!input.contraseña){errors.contraseña='La contraseña es requerida'} 
+//     if(!foundemail){errors.contraseña='Contraseña incorrecta.'}
+//     if(!match){errors.equivocado='Contraseña o  Usuario incorrecto'}
+//     return errors
+// }
 
-export const IniciarSesion=( {users} )=>{
-    const dispatch=useDispatch()
-    const history=useNavigate()
-    const[errors, setErrors]=useState({})
-    const[input, setinput]=useState({ input:'', contraseña:''})
+// export const IniciarSesion=( {users} )=>{
+//     const dispatch=useDispatch()
+//     const history=useNavigate()
+//     const[errors, setErrors]=useState({})
+//     const[input, setinput]=useState({ input:'', contraseña:''})
 
-    // useEffect(()=>{
-    //    setErrors({incompleto:'Formulario incompleto'})     
-    // },[])
+//     // useEffect(()=>{
+//     //    setErrors({incompleto:'Formulario incompleto'})     
+//     // },[])
    
-    // useEffect(()=>{
+//     // useEffect(()=>{
         
-    //  },[errors])
+//     //  },[errors])
     
 
-    const handleInputChange=(e)=>{
-        setinput({...input,
-            [e.target.name]:e.target.value })
-        setErrors(Validate({...input,
-            [e.target.name]:e.target.value},users))
-    } 
+//     const handleInputChange=(e)=>{
+//         setinput({...input,
+//             [e.target.name]:e.target.value })
+//         setErrors(Validate({...input,
+//             [e.target.name]:e.target.value},users))
+//     } 
     
-    const onsubmitHandler= (e)=>{
-        e.preventDefault()
-        if(input.input.length===0 || input.contraseña.length===0 || errors.equivocado ){
-            alert('Formulario incompleto')
-        }
-        else{  
-            dispatch(getUserLoged(input))
-            .then(()=> history(`/home`))
-            .catch(e=>console.log(e))
-        }   
-    }
-    return(
-        <form onSubmit={onsubmitHandler} className='iniciarSeForm'>
-            <div className='inptCont'>
-            <TextField 
-                type="text" 
-                name="input" 
-                value={input.input}                
-                onChange={handleInputChange}
-                sx={{width:'100%',marginTop: '10px',backgroundColor:'white'}} id="outlined-basic" label="Usuario o email" variant="outlined" />
+//     const onsubmitHandler= (e)=>{
+//         e.preventDefault()
+//         if(input.input.length===0 || input.contraseña.length===0 || errors.equivocado ){
+//             alert('Formulario incompleto')
+//         }
+//         else{  
+//             dispatch(getUserLoged(input))
+//             .then(()=> history(`/home`))
+//             .catch(e=>console.log(e))
+//         }   
+//     }
+//     return(
+//         <form onSubmit={onsubmitHandler} className='iniciarSeForm'>
+//             <div className='inptCont'>
+//             <TextField 
+//                 type="text" 
+//                 name="input" 
+//                 value={input.input}                
+//                 onChange={handleInputChange}
+//                 sx={{width:'100%',marginTop: '10px',backgroundColor:'white'}} id="outlined-basic" label="Usuario o email" variant="outlined" />
            
-                {errors.input && (<div className="dangerI">
-                    <img src={'warningWhite.png'} alt='!' className='warning'/>
-                    {errors.input}</div>)}
+//                 {errors.input && (<div className="dangerI">
+//                     <img src={'warningWhite.png'} alt='!' className='warning'/>
+//                     {errors.input}</div>)}
            
-            <TextField 
-                type="password" 
-                name="contraseña" 
-                value={input.contraseña}                
-                onChange={handleInputChange}
-                sx={{width:'100%',marginTop: '10px',backgroundColor:'white'}} id="outlined-basic" label="Contraseña" variant="outlined" />
+//             <TextField 
+//                 type="password" 
+//                 name="contraseña" 
+                             
+//                 onChange={handleInputChange}
+//                 sx={{width:'100%',marginTop: '10px',backgroundColor:'white'}} id="outlined-basic" label="Contraseña" variant="outlined" />
             
-                {errors.contraseña && (<div className="dangerI">
-                    < img src={'warningWhite.png'} alt='!' className='warning'/>
-                    {errors.contraseña}</div>)}
+//                 {errors.contraseña && (<div className="dangerI">
+//                     < img src={'warningWhite.png'} alt='!' className='warning'/>
+//                     {errors.contraseña}</div>)}
             
-            <button  type='submit' className='btnIniSe'>Iniciar Sesión</button>
-            </div>
+//             <button  type='submit' className='btnIniSe'>Iniciar Sesión</button>
+//             </div>
             
-        </form>
+//         </form>
             
-    )
-}
+//     )
+// }
--- src/view/LandingPage/Landing.jsx
+++ src/view/LandingPage/Landing.jsx
@@ -1,7 +1,7 @@
 import './Landing.css'
 import { IniciarSesion } from '../../components/IniciarSesion/InciarSecion'
-import { useEffect, useState } from 'react'
-import { getUsers } from "../../redux/actions/users";
+
+
 import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
 import { display } from "@mui/system";
@@ -24,41 +24,44 @@ export const Landing = () => {
 
     return (
         <div className='landing'>
-            <div className='laCont'>
-            <div className='landArribaCont'>
-                <div className='logo'>SYT</div>
-                <Link to={"/SobreNosotros"}>
-                    <div >
-                        <button className='btnCreCu'>SobreNosotros</button>
-                    </div>        
-                </Link>
+                <div className='laCont'>
+                    <div className='landArribaCont'>
+                             <div className='logo'>SYT</div>
+                            <Link to={"/SobreNosotros"}>
+                                <div >
+                                 <button className='btnCreCu'>SobreNosotros</button>
+                                </div>        
+                            </Link>
 
-                <button onClick={hamdleConfigAccountForm} className='btnCreCu'>Crear cuenta</button>
-            </div>
+                        <button  className='btnCreCu'>Crear cuenta</button>
+                    </div>
+               </div>
             <div className='lan1Cont'>
                 <div className='lan1Left'>
-                    <div className='titleLandCont'>
-                    <img className='sytTitle' alt='img' src={'title.png'}/>
-                <div className='landArribaCont'>
-                    <div className='logo'>SYT</div>
-                </div>
+                        <div className='titleLandCont'>
+                        <img className='sytTitle' alt='img' src={'title.png'}/>
+                        <div className='landArribaCont'>
+                        <div className='logo'>SYT</div>
+                     </div>
                 <div className='lan1Cont'>
                     <div className='lan1Left'>
-                        <div className='titleLandCont'>
-                            <img className='sytTitle' alt='img' src={'title.png'} />
-                        </div>
-                        <div className='landDesc'>
-                            <h5>No hay duda de que puedes beneficiarte en un foro de desarrolladores web. El desafío es encontrar la comunidad de programación o desarrollo web adecuada para sus necesidades.Y aqui tenemos la mejor opcion:
-                                Unete a la Comunidad más grande de desarrolladores de software</h5>
-                            <h5>HAZ CLICK AQUI PARA SER PARTE</h5>
-                            <button onClick={loginWithRedirect} className='btnCreCu' > UNETE </button>
-                        </div>
+                                <div className='titleLandCont'>
+                                    <img className='sytTitle' alt='img' src={'title.png'} />
+                                </div>
+                                <div className='landDesc'>
+                                 <h5>No hay duda de que puedes beneficiarte en un foro de desarrolladores web. El desafío es encontrar la comunidad de programación o desarrollo web adecuada para sus necesidades.Y aqui tenemos la mejor opcion:
+                                     Unete a la Comunidad más grande de desarrolladores de software</h5>
+                                    <h5>HAZ CLICK AQUI PARA SER PARTE</h5>
+                                <button onClick={loginWithRedirect} className='btnCreCu' > UNETE </button>
+                                </div>
                     </div>
                     <div className='lan2rigth'>
                         <img className='laRiImg' alt='img' src={'riri.png'} />
                     </div>
                 </div>
             </div>
+            </div>
+        </div>
         </div>
     )
 }
